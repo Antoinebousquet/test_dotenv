1c07c5efea736f14e81c235398f992fe5837c1e9
Antoinebousquet <moodlinkproject@gmail.com>
2019-10-09 15:03:13 +0200

    troisième commit

--- lib/app.rb
+++ lib/app.rb
@@ -1,7 +1,16 @@
+require 'twitter'
+
 require 'dotenv'
+Dotenv.load
 
-Dotenv.load('.env')
+	client = Twitter::REST::Client.new do |config|
+	  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+	  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+	  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+	  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+	end
 
-puts ENV['TWITTER_API_SECRET']
 
-puts ENV['BEST_WEBSITE_EVER']
+puts client
+
+client.update('Mon premier tweet en Ruby (A) !!!!')
